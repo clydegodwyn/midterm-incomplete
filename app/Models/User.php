cb3a5413562aa81d04938aa0c91e84c002d68992
unknown <naduma.clyde1123@gmail.com>
2023-04-28 23:06:16 -0700

    exam incomplete

---
+++ app/Models/User.php
@@ -0,0 +1,14 @@
+<?php
+
+    namespace App\Models;
+    use Illuminate\Database\Eloquent\Model;
+
+    class User extends Model{
+        protected $table = 'tblstudent';
+        protected $fillable = [
+            'studid','lastname', 'firstname', 'middlename', 'bday', 'age'
+        ];
+
+        public $timestamps = false;
+    }
+
